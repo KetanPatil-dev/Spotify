79a47e773b4a0208f28448c424ea562695eb8663
ketan <mail.ketan027@gmail.com>
2025-07-04 20:30:16 +0530

    Implemented AuthController

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,23 @@
+import UserModel from "../models/user.model.js"
 
-export const Signup=async(req ,res)=>{
+export const authCallback=async(req ,res)=>{
     try{
-        const {fullName,imageUrl,clerkId}=req.b
+        const {id,firstName,lastName,imageUrl}=req.body
+        const user= await UserModel.findOne({clerkId:id})
+        if(!user)
+            {
+                await UserModel.create({
+                    clerkId:id,
+                    fullName:`${firstName} ${lastName}`,
+                    imageUrl,
+
+                })
+            }
+            return res.status(201).json({success:true})
 
     } catch(error)
     {
-        console.log("Signup Error",error)
+        console.log("AuthCallback Error",error)
+        return res.status(500).json({success:false,message:"Internal Server Error"})
     }
 }
