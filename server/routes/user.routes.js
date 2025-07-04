3e9ed58a962f64811c989052c43869ed7ed14626
ketan <mail.ketan027@gmail.com>
2025-07-04 20:34:36 +0530

    Implemented AuthRoutes

---
+++ server/routes/user.routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { authCallback } from "../controllers/user.controller.js";
+const AuthRoutes = express.Router();
+AuthRoutes.post("/callback", authCallback);
+export default AuthRoutes;
