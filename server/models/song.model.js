0dc77d7e89d8a22f0d8785dc86eef9eaf34dd73c
ketan <mail.ketan027@gmail.com>
2025-07-10 10:54:32 +0530

    Added song models

--- server/models/song.model.js
+++ server/models/song.model.js
@@ -14,6 +14,10 @@ const songSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+     audioUrl: {
+      type: String,
+      required: true,
+    },
     duration: {
       type: Number,
       required: true,
